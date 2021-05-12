b40435c6c0ff797a9b8ea6fe2b5334d0302d8398
Brett Jones <bhjones4545@gmail.com>
2021-05-12 16:05:39 -0600

    add classroom lib and spec file

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,26 @@
 class Classroom
+  attr_reader :subject, :capacity, :students
+
+  def initialize(subject, capacity)
+    @subject = subject
+    @capacity = capacity
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+  end
+
+  def yell_at_students
+    @students.map(&:upcase)
+  end
+
+  def over_capactity
+    @capacity >= 4
+  end
+
+  def kick_out
+    @students.index(0)
+  end
+
 end
